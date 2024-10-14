07ccd44d88c287c1b84b1c4cc57b7fddaf0b0921
jamesattoh <koss.attoh27@gmail.com>
2024-10-14 15:21:38 +0100

    we made a comment clickable and created a templage page called layout

--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -1,33 +1,28 @@
+<?php $title = "Le Blog de l'AVBN" ;?> <!--we attribute this title to the variable according to layout.php code -->
 
-<!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="utf-8" />
-      <title>Le blog de l'AVBN</title>
-      <link href="style.css" rel="stylesheet"/>
-   </head>
+<?php ob_start(); ?> <!-- this function enters in memory all the html code which is next-->
+   <h1>Le super blog de l'AVBN !</h1>
+   <p>Derniers billets du blog :</p>
 
-   <body>
-      <h1>Le super blog de l'AVBN !</h1>
-      <p>Derniers billets du blog :</p>
+   <?php 
+   foreach($posts as $post) {
+   ?>
 
-      <?php 
-      foreach($posts as $post) {
-      ?>
+      <div class="news">
+         <h3>
+               <?= htmlspecialchars($post['title']); ?> <!-- the use of "short open tags" or "short eco tags" syntax -->
+               <em>le <?= $post['french_creation_date']; ?></em>
+         </h3>
+               <p>
+               <?= nl2br(htmlspecialchars($post['content'])); ?> <!-- // we display the post content  -->
+               <br/>
+               <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em> <!-- urlencode is to encode a string (character string) in url -->
+               </p>
+      </div>
+   <?php
+   }// tthe end of the posts loop
+   ?>
 
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']); ?> //use of "short open tags" or "short eco tags" syntax
-                <em>le <?= $post['french_creation_date']; ?></em>
-            </h3>
-                <p>
-                <?= nl2br(htmlspecialchars($post['content'])); ?> // we display the post content 
-                <br/>
-                <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-      <?php
-      }// tthe end of the posts loop
-      ?>
-   </body>
-</html>
+<?php $content = ob_get_clean(); ?> <!--with this line, we retrieve all the content generated in the variable $content -->
+
+<?php require('templates/layout.php') ?>
---
+++ blog/templates/layout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <title><?= $title ?></title> <!--//we create a variable to retrieve the title of a page-->
+      <link href="style.css" rel="stylesheet" /> 
+   </head>
+
+   <body>
+      <?= $content ?> <!-- we create a variable to retrieve the content of a body -->
+   </body>
+</html>
